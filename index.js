c5623a9cde361cb02e043607862a871389573b14
Kelly Duran <kaduran16@gmail.com>
2022-06-29 11:43:38 -0400

    Done with this assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    //Need to call by ID "text" and replace text with "This is really cool!"
+    //Not sure if I should use getElement or querySelector for p#text
+    const newText = document.getElementById("text"); 
+    newText.textContent ="This is really cool!";
+    
+  });
+
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+  
